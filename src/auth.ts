a69ebae4fc6192a1a0e5976d592185ebc7716cf6
dayvidwhy <dayvidwhy@gmail.com>
2024-04-01 20:54:16 +1000

    Add octokit to pull data from github. Grab github access token from login flow.

--- src/auth.ts
+++ src/auth.ts
@@ -1,13 +1,29 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
 import GitHub from "@auth/sveltekit/providers/github";
 import { GITHUB_CLIENT_ID, GITHUB_SECRET } from "$env/static/private";
-console.log(GITHUB_CLIENT_ID, GITHUB_SECRET);
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
     providers: [
         GitHub({
             clientId: GITHUB_CLIENT_ID,
-            clientSecret: GITHUB_SECRET
+            clientSecret: GITHUB_SECRET,
         })
     ],
+    callbacks: {
+        async jwt({ token, account }) {
+            if (account && account.access_token) {
+                token.accessToken = account.access_token;
+            }
+            return token;
+        },
+        async session({ token, session }) {
+            return {
+                ...session,
+                accessToken: token.accessToken
+            };
+        }
+    },
+    session: {
+        strategy: "jwt"
+    }
 });
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,3 +1,56 @@
+<script lang="ts">
+import { page } from "$app/stores";
+import { Octokit } from "@octokit/core";
+import type { Endpoints } from "@octokit/types";
+
+type listUserReposParameters = Endpoints["GET /user"]["response"];
+type listUserReposResponse = Endpoints["GET /users/{username}/repos"]["response"];
+
+const fetchLanguageData = async () => {
+    const octokit = new Octokit({
+        // @ts-ignore
+        auth: $page.data.session?.accessToken || null
+    });
+    const { data }: listUserReposParameters = await octokit.request("GET /user");
+    console.log(data);
+
+    const result: listUserReposResponse = await octokit.request("GET /users/{username}/repos", {
+        username: data.login
+    });
+    // languages returns how man bytes of each language file
+    const repositoryLanguages = await Promise.all(result.data.map((repo) => (
+        octokit.request("GET /repos/{owner}/{repo}/languages", {
+            owner: data.login,
+            repo: repo.name
+        })
+    )));
+
+    // totals languages across the projects
+    const totals: any = {};
+    let byteTotal = 0;
+    repositoryLanguages.forEach(({ data }) => {
+        Object.keys(data).forEach((key) => {
+            if (totals[key] === undefined) {
+                totals[key] = 0;
+            }
+            totals[key] += data[key];
+            byteTotal += data[key];
+        });
+    });
+
+    return {
+        totals,
+        byteTotal
+    };
+};
+</script>
+
 <h1>
     Dashboard page
 </h1>
+<p>
+    Data loaded
+    {#await fetchLanguageData() then data}
+        {JSON.stringify(data)}
+    {/await}
+</p>
