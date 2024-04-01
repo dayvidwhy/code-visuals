babe5bebcae417178ee0acc9d8af336183b1ccc9
dayvidwhy <dayvidwhy@gmail.com>
2024-04-01 15:24:09 +1000

    Navigation between home and dashboard.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,33 @@
-<script>
+<script lang="ts">
     import "../app.css";
+    import { page } from '$app/stores';  
+
+    const links = [
+        {
+            name: 'Home',
+            href: '/'
+        },
+        {
+            name: 'Dashboard',
+            href: '/dashboard'
+        }
+    ];
 </script>
 
 <div>
     <nav>
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/dashboard">Dashboard</a></li>
+        <ul class="flex">
+            {#each links as link}
+                <li class={`${$page.url.pathname === link.href ? "border-b-2 border-slate-600" : ""}`}>
+                    <a class="block py-2 px-2" href={link.href}>{link.name}</a>
+                </li>
+            {/each}
         </ul>
     </nav>
-    <slot>
-    </slot>
+    <div class="container m-auto pt-4">
+        <slot>
+        </slot>
+    </div>
 </div>
 
 
